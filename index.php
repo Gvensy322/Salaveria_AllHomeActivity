f693a692f8fd1286d433464dc58070097b46d9f8
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-08-02 10:51:42 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h>Activity Name:All Home Activity</h>
+<p>Name:Gorven G. Salaveria</p>
+<p>Course Title: Advance Web Development,Third Year</p>
+<p>Activity Name:Activities 01,02,03,04,05</p>
+
+<p>
+<a href=https://salaveriahelloworldwebsite.herokuapp.com/>Link to Activity 01</a>
+</p>
+
+<p>
+<a href=https://salaveriaitmm103homeactivity2.herokuapp.com/>Link to Activity 02</a>
+</p>
+
+<p>
+<a href=https://salaveriaitmm103homeactivity3.herokuapp.com/>Link to Activity 03</a>
+</p>
+
+<p>
+<a href=https://salaveriahelloworldwebsite.herokuapp.com/>Link to Activity 04</a>
+</p>
+
+<p>
+<a href=https://salaveriaitmm103homeactivity5.herokuapp.com/>Link to Activity 05</a>
+</p>
+
+
+
+
+</body>
+</html>
