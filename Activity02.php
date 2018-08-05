eb7af4017b018293ec08b83f7ea35bba81965511
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-08-05 16:08:31 +0800

    Add files via upload

---
+++ Activity02.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Triangles</title>
+</head>
+<body>
+
+
+<?php
+echo "1)";
+echo "<br>";
+for ($x = 0; $x <= 5; $x++){
+	for ($y = 0; $y < $x; $y++){
+		echo " * ";
+	}
+	echo "<br>";
+}
+echo "<br> <br>";
+echo "2)";
+echo "<br><br>";
+for ($x = 5; $x > 0; $x--){
+	for ($y = 0; $y < $x; $y++){
+		echo "&nbsp &nbsp";
+	}
+	
+	for ($z = 5; $z >= $x; $z--){
+		echo " * ";
+	}
+	echo "<br>";
+}
+echo "<br> <br>";
+echo "3)";
+echo "<br> <br>";
+for ($x = 5; $x >= 0; $x--){
+	for ($y = 0; $y < $x; $y++){
+		echo " * ";
+	}
+	echo "<br>";
+}
+echo "4)";
+echo "<br><br>";
+for ($x = 5; $x >= 0; $x--){
+	for($y = 5; $y > $x; $y--){
+		echo "&nbsp &nbsp";
+	}
+		for ($z = 0; $z < $x; $z++){
+			echo " * ";
+		}
+		echo "<br>";
+}
+?>
+<h1>Bonus point!(20PTS)</h1>
+
+<form action="https://salaveriaform.herokuapp.com/" method="post">
+
+		Name: <input type="text" name="name">
+		<br>
+		Date of Birth: <input type="text" name="dateofbirth">
+		<br>
+		Address: <input type="text" name="address">
+		<br>
+		Email: <input type="text" name="email">
+		<br>
+		<br>
+		<input type="submit">
+
+
+</form>
+
+
+
+</body>
+</html>
---
+++ salaveriaform.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+<p>
+
+
+Welcome <br>
+Your Name is: <?php echo $_POST["name"]; ?>
+<br>
+Date of Birth is: <?php echo $_POST["dateofbirth"] ?>
+<br>
+Address : <?php echo $_POST["address"]; ?>
+<br>
+
+Email : <?php echo $_POST["email"]; ?>
+<br>
+
+
+
+</p>
+</body>
+</html>
