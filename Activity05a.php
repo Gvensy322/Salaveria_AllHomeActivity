4d4ab17a3a5f510f4e6b263028360b73a60aefe8
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-08-07 09:17:16 +0800

    Add files via upload

---
+++ Activity05a.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+	
+<title>Calculate Age</title>
+</head>
+	
+<body>
+<form action = "compute.php" method="post">
+	
+	<h>Activity 5a</h>
+	<h4>Calculate Age from Date of Birth Using PHP</h4>
+	
+
+
+<input type="submit" name="submit" value="Click Here"/>
+	
+	<br>
+	<br>
+	<h>Activity 5b </h>
+	<br>
+	<a href=Activity5b.php>Link to Activity 5b</a>
+	
+	
+</form>
+
+		
+
+
+
+	
+
+		
+</body>
+</html>
---
+++ Activity5b.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <head>
+    <title>Read,Display,Create New Line Hello World</title>
+  </head>
+
+  <h>Activity 5b.1</h>
+  <br>
+  <br>
+
+<?php
+echo readfile("HelloWorld.txt");
+?>
+  <br>
+  <br>
+
+  
+  <h>Activity 5b.2</h>
+  <br>
+  <br>
+  
+ <?php 
+for ($x = 0; $x <= 10; $x++) {
+    echo "Hello World <br>";
+} 
+  
+?>
+  <br>
+  <br>
+    <h>Activity 5b.3</h>
+  <br>
+  <br>
+
+<?php
+echo readfile("NewHelloWorld.txt");
+?>
+  
+  
+  
+
+</body>
+</html>
