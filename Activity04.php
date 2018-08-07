40f57e3afdc2d10bc081a1cb2e71a8d5eb71c252
Gvensy322 <40430539+Gvensy322@users.noreply.github.com>
2018-08-07 09:14:11 +0800

    Add files via upload

---
+++ Activity04.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="receiver.php" method="post">
+	Username:<br>
+	<input type="text" name="username">
+	<br>
+	Password:<br>
+	<input type="password" name="password">
+	<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ receiver.php
@@ -0,0 +1,7 @@
+<?php
+$user_name = $_POST['username'];
+$pass_word = $_POST['password'];
+echo "Username: " . $user_name;
+echo "<br>";
+echo "Password: " . $pass_word;
+?>
